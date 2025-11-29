88d8b06b7507bb06f4ce5ab126ba4e5a015f66c8
Giahan <kaytlynnnguyen@gmail.com>
2025-11-29 23:31:22 +0700

    Cap nhat project: them xu ly du lieu login, register

--- project/app.js
+++ project/app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 const { engine } = require('express-handlebars');
+const session = require('express-session');
 var app = express();
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -17,6 +18,75 @@ app.engine(
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 var userRouter = require('./routes/users');
+
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const User = require('./models/User');
+const bcryptjs = require('bcryptjs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect('mongodb://127.0.0.1/node')
+    .then(()=>{
+        console.log("MongoDB connected successfully.");
+    })
+    .catch(err=>{
+        console.error("Error connecting to MongoDB",err);
+    });
+app.use(session({
+    secret: 'mySecret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 } // 1 giờ
+}));
+
+app.post('/login', (req, res) => {
+    User.findOne({email: req.body.email}).then((user) => {
+        if (user) {
+            bcryptjs.compare(req.body.password,user.password,(err,matched)=>{
+                if(err) return err;
+                if(matched){
+                    //res.send("User was logged in");
+                    req.session.user =
+                    {
+                        id:user._id,
+                        email:user.email,
+                    };
+                    res.redirect('/');
+                }else {
+                    res.send("Email hoac mat khau khong dung");
+                }
+            });
+        }else{
+            res.send("User khong ton tai");
+        }
+    })
+});
+app.post('/register',  (req,res) => {
+        console.log(req.body);
+        const newUser = new User();
+        newUser.email = req.body.email;
+        newUser.password = req.body.password;
+        bcryptjs.genSalt(10, function (err, salt) {
+            bcryptjs.hash(newUser.password, salt, function (err, hash) {
+                if (err) {return  err}
+                newUser.password = hash;
+
+                newUser.save().then(userSave=>
+                {
+                    res.send('USER SAVED');
+                }).catch(err => {
+                    res.send('USER ERROR'+err);
+                });
+            });
+        });
+    }
+);
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) return res.send("Logout error");
+        res.redirect('/login');
+    });
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -26,6 +96,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// Middleware truyền thông tin user ra view
+app.use((req, res, next) => {
+    res.locals.user = req.session.user || null;
+    next();
+});
 
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
